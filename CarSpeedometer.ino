a1f78549c51617654cde528b3bce3a4b3c5483f7
Horashio <Horashi0@users.noreply.github.com>
2024-02-09 00:51:16 +0000

    Updated CarSpeedometer.ino
    To learn to control the LEDs I decided to make a little program moving the led around on the matrix display

--- CarSpeedometer.ino
+++ CarSpeedometer.ino
@@ -2,14 +2,34 @@
 
 ArduinoLEDMatrix matrix;
 
+int x = 0;
+int y = 0;
+int oldX = 0;
+int oldY = 0;
+
+byte frame[8][12] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+
 void setup() {
   Serial.begin(115200);
   matrix.begin();
+ 
 }
 
 void loop() {
+  /*
   byte frame[8][12] = {
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+
     {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
     {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
     {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0},
@@ -19,4 +39,40 @@ void loop() {
     {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
   };
   matrix.renderBitmap(frame, 8, 12);
+  */
+  if(Serial.available() > 0)
+  {    
+    char byte = Serial.read();
+    Serial.println(byte);
+    
+    oldX = x;
+    oldY = y;
+
+    if(byte == 'L' && y > 0)
+    {
+      y--;
+    }
+    if(byte == 'R' && y < 11)
+    {
+      y++;
+    }
+    if(byte == 'U' && x > 0)
+    {
+      x--;
+    }
+    if(byte == 'D' && x < 7)
+    {
+      x++;
+    }
+
+    Serial.println(y);
+    Serial.println(x);
+
+    frame[oldX][oldY] = 0;
+    frame[x][y] = 1;
+
+    matrix.renderBitmap(frame, 8, 12);
+  }
+  
+
 }
